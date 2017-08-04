7b0abcb05727710d58ff7353802cf2b42005cdca
noah the goodra <peterpan0413@live.com>
2017-08-03 20:44:18 -0500

    add plugin menu thing

---
+++ GenericUnityHelpers/GenericHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GenericHelpersn
+{
+    public static class GenericHelpers
+    {
+        private static GameObject Fakelink;
+
+        public static void CreateGameObjectAndAttachClass<T>() where T : MonoBehaviour
+        {
+            Fakelink = new GameObject();
+            Fakelink.AddComponent<T>();
+            GameObject.DontDestroyOnLoad(Fakelink);
+        }
+
+        public static void CreateGameObjectAndAttachClassAndAllowDestory<T>() where T : MonoBehaviour
+        {
+            Fakelink = new GameObject();
+            Fakelink.AddComponent<T>();
+        }
+    }
+}
